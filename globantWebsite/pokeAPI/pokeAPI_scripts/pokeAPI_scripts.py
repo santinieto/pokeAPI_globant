c158e31a8a04c977609cb88797c9798d2e2cca5b
Santiago Nieto <santi.nieto@live.com>
2022-10-05 13:00:30 -0300

    Agregue la visualizacion de estadisticas y el histograma en el HTML

---
+++ globantWebsite/pokeAPI/pokeAPI_scripts/pokeAPI_scripts.py
@@ -0,0 +1,67 @@
+import requests
+import numpy as np
+import pylab as pl
+
+POKEAPE__MAIN_URL = 'https://pokeapi.co/api/v2/berry/'
+
+def get_berries_number():
+    """Cuento cuantas berries hay"""
+
+    # Pido la respuesta de la API externa
+    response            = requests.get( POKEAPE__MAIN_URL )
+
+    # Un codigo 200 indica que se obtuvieron datos satisfactoriamente
+    try:
+        if response.status_code == 200:
+            payload     = response.json()
+            nberries    = payload.get( 'count', [] )
+            
+            print('There is {} berries in the dataset'.format( nberries ))
+    except:
+        print('ERROR! Something went wrong while loading data')
+
+    return nberries
+
+def get_berries_data(nberries):
+    """Obtengo los nombres y datos de cada berry"""
+
+    # Obtengo los datos de las berries
+    berry_names      = []
+    berry_grow_times = []
+    try:
+        for k in range(nberries+1):
+            response = requests.get( POKEAPE__MAIN_URL + str(k) )
+            
+            if response.status_code == 200:
+                payload             = response.json()
+                berry_name          = payload.get( 'name', [] )
+                berry_growth_time   = payload.get( 'growth_time', [] )
+                print( 'berry {} ---> {} with grow time of {}'.format(k, berry_name, berry_growth_time) )
+                
+                # Guardo los datos en listas:
+                berry_names.append( berry_name )
+                berry_grow_times.append( berry_growth_time )
+    except:
+        print('ERROR! Something went wrong while loading data')
+
+    return berry_names, berry_grow_times
+
+def plot_berries_hist(berry_grow_times):
+    """Hago un histograma para mostrar los datos""" 
+
+    # Genero el grafico
+    pl.figure()
+    pl.hist(berry_grow_times)
+    pl.grid()
+    pl.xlabel('Berri Grow Time')
+    pl.ylabel('Ocurrences')
+    pl.title("Berri's Grow Times Histogram")
+   
+    # Guardo la figura
+    pl.savefig("./pokeAPI/static/img/berries_hist.png", dpi = 100)
+
+    # Cierro la figura
+    pl.close()
+
+if __name__ == '__main__':
+    get_berries_number()
--- globantWebsite/pokeAPI/views.py
+++ globantWebsite/pokeAPI/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from .pokeAPI_scripts.pokeAPI_scripts import *
+import numpy as np
 
 # Create your views here.
 def index(request):
@@ -14,6 +16,35 @@ def index(request):
 
 def allBerryStats(request):
 
+    # Obtengo los datos de las berries
+    nberries = 5 # get_berries_number()
+    berry_names, berry_grow_times = get_berries_data(nberries)
+
+    # Convierto los valores de crecimiento en un arreglo de numpy
+    berry_grow_times = np.asarray( berry_grow_times )
+
+    # Creo el diccionario con la informacion
+    unique, counts = np.unique(berry_grow_times, return_counts=True)
+    frequency = list(zip(unique, counts))
+
+    berries_dicc = {
+        #"berries_names": berry_names,
+        "min_growth_time": str( min( berry_grow_times ) ),
+        "median_growth_time": str( np.median( berry_grow_times ) ),
+        "max_growth_time": str( max( berry_grow_times ) ),
+        "variance_growth_time": str( np.var( berry_grow_times ) ),
+        "mean_growth_time": str( np.mean( berry_grow_times ) ),
+        "frequency_growth_time": str( frequency )
+    }
+
+    # Genero el grafico
+    plot_berries_hist(berry_grow_times)
+
+    # Hago el render
     return render(request, 'all_berry_stats.html', {
         'title' : 'Estadísticas de las berries',
+        'nberries' : nberries,
+        'berry_names' : berry_names,
+        'berry_grow_times' : berry_grow_times,
+        'berries_dicc' : berries_dicc,
     })
