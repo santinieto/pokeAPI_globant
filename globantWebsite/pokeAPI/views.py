d4d167b93756130d54c33cc9478d89266909ed36
Santiago Nieto <santi.nieto@live.com>
2022-10-05 10:51:21 -0300

    Added basic views to project (index and navbar HTMLs)

--- globantWebsite/pokeAPI/views.py
+++ globantWebsite/pokeAPI/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("""
-    <h1>Index page</h1>
-    """)
+    # Basic HTTP response
+    # return HttpResponse("""
+    # <h1>Index page</h1>
+    # """)
+
+    # Render response
+    return render(request, 'index.html', {
+        'title' : 'Pagina principal - pokeAPI',
+    })
